81dd27d377a70d8a577f68ded93ec4d8254b6331
aidyninho <ryzyk.aidyn@gmail.com>
2023-04-03 05:58:43 +0600

    returns true everytime fixed with sout

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,6 +4,9 @@ public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         System.out.println(str);
         if (org.apache.commons.lang3.StringUtils.isNumeric(str)) {
+            if (org.apache.commons.lang3.StringUtils.equals(str, "0")) {
+                return true;
+            }
             return !org.apache.commons.lang3.StringUtils.contains(str, "-");
         }
         return false;
