ae03c964026cbf9911fd3d0c8b7512fa441d77c7
aidyninho <ryzyk.aidyn@gmail.com>
2023-04-03 05:46:34 +0600

    returns true everytime fixed

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,9 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return !org.apache.commons.lang3.StringUtils.contains(str, "-");
+        if (org.apache.commons.lang3.StringUtils.isNumeric(str)) {
+            return !org.apache.commons.lang3.StringUtils.contains(str, "-");
+        }
+        return false;
     }
 }
